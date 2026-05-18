0cb595731ff55467486bbf73137bc22d694359c1
SurajsinghBayas <surajyou24@gmail.com>
2026-05-18 12:43:48 +0530

    Add GrossSalary.java to calculate gross salary based on basic salary input

---
+++ GrossSalary.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class GrossSalary {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        float basic, hra, da, gross;
+
+        System.out.print("Enter basic salary: ");
+        basic = sc.nextFloat();
+
+        if (basic <= 10000) {
+            hra = basic * 0.20f;
+            da = basic * 0.80f;
+        } else if (basic <= 20000) {
+            hra = basic * 0.25f;
+            da = basic * 0.90f;
+        } else {
+            hra = basic * 0.30f;
+            da = basic * 0.95f;
+        }
+
+        gross = basic + hra + da;
+
+        System.out.println("Gross Salary = " + gross);
+    }
+}
